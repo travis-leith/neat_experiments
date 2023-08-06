b9696248c5084667803f1774e44f00399ae6a2c8
Travis <travisgleith@gmail.com>
2023-08-06 19:56:42 +0400

    does not compile - wip

--- src/neat/common.rs
+++ src/neat/common.rs
@@ -11,14 +11,8 @@ pub struct Connection {
 }
 
 impl Connection {
-    fn new(in_node_id: usize, out_node_id: usize, weight: f64, innovation_num: usize) -> Connection {
-        Connection{
-            in_node_id,
-            out_node_id,
-            weight,
-            innovation_num,
-            enabled: true
-        }
+    fn copy_conn(&self) -> Connection {
+        Connection { in_node_id: self.in_node_id, out_node_id: self.out_node_id, weight: self.weight, innovation_num: self.innovation_num, enabled: self.enabled }
     }
 }
 
@@ -27,6 +21,7 @@ pub struct Network {
     n_output_nodes: usize,
     n_hidden_nodes: usize,
     genome: Vec<Connection>,
+    genome_size: usize,
     input_connections_map: Vec<Vec<usize>>,
     nodes_with_active_inputs: Vec<bool>,
     active_nodes: Vec<bool>,
@@ -47,6 +42,7 @@ impl Network {
             n_output_nodes,
             n_hidden_nodes: 0,
             genome: Vec::new(),
+            genome_size: 0,
             input_connections_map: Vec::new(),
             active_nodes,
             nodes_with_active_inputs: vec![false; n_output_nodes], //sensors do not have inputs so no need to have values for them here 
@@ -55,14 +51,19 @@ impl Network {
         }
     }
 
-    fn new(n_sensor_nodes: usize, n_output_nodes: usize, innovation_map: HashMap<(usize, usize), usize>) -> Network {
+    fn create_from_genome(n_sensor_nodes: usize, n_output_nodes: usize, genome: Vec<Connection>) -> Network {
+        //need to determine how many input nodes
+    }
+
+    fn init_from_starting_innovation_map(n_sensor_nodes: usize, n_output_nodes: usize, innovation_map: HashMap<(usize, usize), usize>) -> Network {
         let mut res = Network::empty(n_sensor_nodes, n_output_nodes);
         let between = Uniform::from(-1.0..1.0);
         let mut rng = rand::thread_rng();
         for ((in_node_id, out_node_id), innovation_num) in innovation_map.iter() {
             let weight = between.sample(&mut rng);
-            res.genome.push(Connection::new(*in_node_id, *out_node_id, weight, *innovation_num));
+            res.genome.push(Connection { in_node_id: *in_node_id, out_node_id: *out_node_id, weight, innovation_num: *innovation_num, enabled: true });
         }
+        res.genome_size = innovation_map.len();
         res.set_phenotype()
     }
 
@@ -102,6 +103,81 @@ impl Network {
         self
     }
 }
+
+pub struct Organism {
+    network: Network,
+    fitness: usize // consider changing to generic type
+}
+
+fn cross_over(organism_1: &Organism, organism_2: &Organism) -> Organism {
+    let mut gene_index_1 = 0;
+    let mut gene_index_2 = 0;
+    let genome_size_1 = organism_1.network.genome_size;
+    let genome_size_2 = organism_2.network.genome_size;
+    let max_genome_size = std::cmp::max(genome_size_2, genome_size_1);
+    let mut gene_pair_index = 0;
+    let mut gene_pairs = Vec::with_capacity(max_genome_size);
+
+    while gene_index_1 < genome_size_1 || gene_index_2 < genome_size_2 {
+        if gene_index_1 < genome_size_1 {
+            if gene_index_2 < genome_size_2 {
+                //still processing org_1 and org_2
+                if organism_1.network.genome[gene_index_1].innovation_num == organism_2.network.genome[gene_index_2].innovation_num {
+                    gene_pairs[gene_pair_index] = (Some(&organism_1.network.genome[gene_index_1]), Some(&organism_2.network.genome[gene_index_2]));
+                    gene_pair_index+=1;
+                    gene_index_1+=1;
+                    gene_index_2+=1;
+                } else if organism_1.network.genome[gene_index_1].innovation_num < organism_2.network.genome[gene_index_2].innovation_num {
+                    gene_pairs[gene_pair_index] = (Some(&organism_1.network.genome[gene_index_1]), None);
+                    gene_pair_index+=1;
+                    gene_index_1+=1;
+                } else {
+                    gene_pairs[gene_pair_index] = (None, Some(&organism_2.network.genome[gene_index_2]));
+                    gene_pair_index+=1;
+                    gene_index_2+=1;
+                }
+            } else {
+                //still processing org_1 but finished with org_2
+                gene_pairs[gene_pair_index] = (Some(&organism_1.network.genome[gene_index_1]), None);
+                gene_pair_index+=1;
+                gene_index_1+=1;
+            }
+        } else if gene_index_2 < genome_size_2 {
+            //finished processing org_1 but still busy with org_2
+            gene_pairs[gene_pair_index] = (None, Some(&organism_2.network.genome[gene_index_2]));
+            gene_pair_index+=1;
+            gene_index_2+=1;
+        } else {
+            //if we are finished with org_1 and org_2, why are we still here?
+            unreachable!("Cross over did not complete when expected")
+        }
+    }
+    let between = Uniform::from(0.0..1.0);
+    let mut rng = rand::thread_rng();
+    let new_genome : Vec<Connection> = gene_pairs.into_iter().map(|pair| {
+        let r = between.sample(&mut rng);
+        match pair {
+            (Some(conn1), Some(_)) if r > 0.5 => Some(conn1.copy_conn()),
+            (Some(_), Some(conn2)) => Some(conn2.copy_conn()),
+            (Some(conn1), None) if organism_1.fitness > organism_2.fitness => Some(conn1.copy_conn()),
+            (Some(_), None) if organism_1.fitness < organism_2.fitness => None,
+            (Some(conn1), None) if r > 0.5 => Some(conn1.copy_conn()),
+            (Some(_), None) => None,
+            (None, Some(_)) if organism_1.fitness > organism_2.fitness => None,
+            (None, Some(conn2)) if organism_1.fitness < organism_2.fitness => Some(conn2.copy_conn()),
+            (None, Some(_)) if r > 0.5 => None,
+            (None, Some(conn2)) => Some(conn2.copy_conn()),
+            (None, None) => unreachable!("Cross over did not complete when expected")
+        }
+    }).flatten().collect();
+    let res = Organism { 
+        network: Network::empty(1,1),
+        fitness: 0
+     };
+    res
+    // organism_1
+}
+
 fn relu(x: f64) -> f64 {
     if x > 0.0 {
         x
@@ -154,15 +230,7 @@ fn add_connection(mut network: Network, in_id: usize, out_id: usize, weight: f64
         panic!("Tried to add a connection with an output ({out_id}) that skips an available id ({})", network.n_total_nodes())
     }
 
-    let new_conn = Connection{
-        in_node_id: in_id,
-        out_node_id: out_id,
-        weight: weight,
-        innovation_num: global_innovation,
-        enabled: true
-    };
-
-    network.genome.push(new_conn);
+    network.genome.push(Connection { in_node_id: in_id, out_node_id: out_id, weight, innovation_num: global_innovation, enabled: true });
     (network, global_innovation + 1)
 }
 
@@ -314,4 +382,6 @@ mod tests {
         let new_network = network.activate(vec![-0.9, 0.6]);
         assert_approx_eq!(new_network.node_values[2], 0.0216);
     }
+
+
 }
